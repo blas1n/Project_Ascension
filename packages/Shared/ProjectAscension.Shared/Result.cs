a969c47aa7e66174b7df6d1fa3f89f7100c0bfc2
blasin <blasin@npixel.co.kr>
2026-06-22 17:07:21 +0900

    feat: add Shared package with Result<T> and Error types

---
+++ packages/Shared/ProjectAscension.Shared/Result.cs
@@ -0,0 +1,14 @@
+namespace ProjectAscension.Shared;
+
+public class Result<T>
+{
+    public T? Value { get; }
+    public Error Error { get; }
+    public bool IsSuccess => Error == Error.None;
+
+    private Result(T value) { Value = value; Error = Error.None; }
+    private Result(Error error) { Error = error; }
+
+    public static Result<T> Ok(T value) => new(value);
+    public static Result<T> Fail(Error error) => new(error);
+}
